f08e13e3fd5624214afa14ce345c3dc01b0d0532
Laura Fitzgerald <laurafitzgeraldsemail@gmail.com>
2015-11-21 10:42:18 +0000

    start documentation on isbn search

--- test_database_with_cache.rb
+++ test_database_with_cache.rb
@@ -13,7 +13,7 @@ describe DatabaseWithCache do
       context "Given the book ISBN is valid" do
         context "and it is not in the local cache" do
           context "nor in the remote cache" do
-              it "should read it from the d/b and add it to the remots cache" do
+              it "should read it from the d/b and add it to the remote cache" do #should add to the local cache as well
                  expect(@memcached_mock).to receive(:get).with('v_1111').and_return nil
                  expect(@memcached_mock).to receive(:set).with('v_1111',1)
                  expect(@memcached_mock).to receive(:set).with('1111_1',@book1111.to_cache)
@@ -37,10 +37,20 @@ describe DatabaseWithCache do
         end        
         context "it is in the local cache" do
            context "and up to date with the remote cache" do
-              it "should use the local cache version"
-          end   
+              it "should use the local cache version" do
+              end
+           end
+           context "and not up to date with the remote cache" do
+              it "should use the "
+
+
+           end   
         end
       end
+      context "Give the book ISBN is not valid" do
+
+      end
+
     end
     
 end
